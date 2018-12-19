348a4d132501fd6e719443a6b571eb96ae4f9536
mirmoboy <kevinbatongbakal@Rheas.local>
2018-12-19 22:46:25 +0800

    Update gitignore

---
+++ dist/precache-manifest.23dd8338c64a3f35d5a2e5f488a4cc2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c2b3a1c8f4c42c931c93",
+    "url": "/js/chunk-vendors.259ee540.js"
+  },
+  {
+    "revision": "92fbae8c2521a5fd9cca",
+    "url": "/js/app.38d0c535.js"
+  },
+  {
+    "revision": "b75f5b749c33bdf42a37",
+    "url": "/js/about.4ee2142b.js"
+  },
+  {
+    "revision": "12f135d5f85868f4c23c41fbd6c9553f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2b3a1c8f4c42c931c93",
+    "url": "/css/chunk-vendors.96d1351d.css"
+  },
+  {
+    "revision": "92fbae8c2521a5fd9cca",
+    "url": "/css/app.219ac057.css"
+  }
+];
