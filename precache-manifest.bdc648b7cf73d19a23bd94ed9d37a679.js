9caff74f65be6fe1e47275b6a6dbc719f5979a9e
mirmoboy <kevinbatongbakal@Rheas.local>
2018-12-23 15:48:54 +0800

    Update

---
+++ precache-manifest.bdc648b7cf73d19a23bd94ed9d37a679.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1f7a6d030a1fa7be697a",
+    "url": "/js/chunk-vendors.dafb8116.js"
+  },
+  {
+    "revision": "411ec10b73a35c1e607e",
+    "url": "/js/chunk-2d21dbcb.fe2e4efb.js"
+  },
+  {
+    "revision": "9a11c2e55b5ecbcd39c5",
+    "url": "/js/chunk-2d2138f6.1ace1781.js"
+  },
+  {
+    "revision": "dddbf0c8b5af83424def",
+    "url": "/js/chunk-2d213754.f2312636.js"
+  },
+  {
+    "revision": "86f8a580964f80420b22",
+    "url": "/js/chunk-2d0c1283.efc6cc17.js"
+  },
+  {
+    "revision": "347dd1651d2a3064fb2e",
+    "url": "/js/app.c33b5fdb.js"
+  },
+  {
+    "revision": "7cbf5ac48c93e0c7406c3b4d16e25ce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5befc73bcff54101a5a10072ebcf0aa",
+    "url": "/img/bulma-white.d5befc73.png"
+  },
+  {
+    "revision": "347dd1651d2a3064fb2e",
+    "url": "/css/app.bcec30da.css"
+  }
+];
