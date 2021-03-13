8a01e37eeb0b14334ca8f03e4b2ed5c125f442b6
Kevin Batongbakal <kbatongbakal>
2021-03-13 20:04:44 +0800

    feat: Update exp and project

---
+++ precache-manifest.50458c3a977cd43bb1aff067b7ade53f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3e5bb325fc695e50aabd",
+    "url": "/js/chunk-vendors.ceb7a6c4.js"
+  },
+  {
+    "revision": "a230f25135148e800e88",
+    "url": "/js/chunk-2d21dbcb.80013e8c.js"
+  },
+  {
+    "revision": "6fd4b63bda0ddc936c9e",
+    "url": "/js/chunk-2d2138f6.6b7de726.js"
+  },
+  {
+    "revision": "b1a1093cba41769ac18e",
+    "url": "/js/chunk-2d213754.5b5f6742.js"
+  },
+  {
+    "revision": "a849f8c3c2792d392946",
+    "url": "/js/chunk-2d0c1283.9b09818e.js"
+  },
+  {
+    "revision": "6cabf74078360a354bb3",
+    "url": "/js/app.6b445140.js"
+  },
+  {
+    "revision": "734cbdac0ca854bd8476ca5c488c16aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5befc73bcff54101a5a10072ebcf0aa",
+    "url": "/img/bulma-white.d5befc73.png"
+  },
+  {
+    "revision": "6cabf74078360a354bb3",
+    "url": "/css/app.650595e6.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7adf9421f78f81046259c72b07239b51.js"
+  "/precache-manifest.50458c3a977cd43bb1aff067b7ade53f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "mirmoboy.github.io"});
